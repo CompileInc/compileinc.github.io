23e611ea3a73d5e0d5587df6c1ae2eadafcd37f6
Cyriac Thomas <cyriac@compile.com>
2018-04-02 20:02:15 +0530

    nuxtjs port

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Portfolio powered by github API
+
 Source code for the code portfolio on github powered by github API.
 
-If you want your own portfolio, fork the project and change the ```github_username``` in ```_config.yml``` and change the repo-name appropriately as mentioned [here](https://help.github.com/articles/user-organization-and-project-pages/).
+If you want your own portfolio, fork the project and change the `github_username` in nuxt.config.json and change the repo-name appropriately as mentioned [here](https://help.github.com/articles/user-organization-and-project-pages/).
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <div class="container">
+        <app-logo/>
+      </div>
+      <div class="jumbotron jumbotron-fluid bg-primary text-white">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-xl-8">
+              <h1 class="display-4">Hello World!</h1>
+              <p class="lead">Open source projects from <a href="https://www.compile.com" target="_blank">Compile</a> and the forks of tools we use.</p>
+            </div>
+            <div class="col-12 col-lg-6 col-xl-4">
+              <UserInfo />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <RepoListing />
+      </div>
+    </div>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import UserInfo from '~/components/UserInfo.vue'
+import RepoListing from '~/components/RepoListing.vue'
+
+export default {
+  components: {
+    AppLogo,
+    UserInfo,
+    RepoListing
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.jumbotron {
+  border-radius: 0;
+  min-height: 50vh;
+  a {
+    color: #FFF;
+    text-decoration: none;
+    border-bottom: 1px dotted #FFF;
+  }
+  &.bg-primary {
+    h1 {
+      color: #FFF;
+      font-weight: 800
+    }
+  }
+}
+</style>
